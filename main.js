4e06c9d92a4e0a005f63215d3eadac633c19d1df
ThanongsakHomthai <77135181+ThanongsakHomthai@users.noreply.github.com>
2021-04-26 16:57:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -18,55 +18,6 @@
   
   
     /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    /*==================================================================
     [ Show pass ]*/
     var showPass = 0;
     $('.btn-show-pass').on('click', function(){
@@ -88,6 +39,7 @@
 
 })(jQuery);
 
+var alertText = document.getElementById("textalert");
 var username = "thanongsakhom.gas@gmail.com"
 var username1 = "admin"
 var password = "1234"
@@ -98,9 +50,11 @@ document.getElementById("form-login").onsubmit = function(e){
     if(user ===username && pass === password || user ===username1 && pass === password ){
         localStorage.setItem("username",user)
         window.location = "home1.html"
+        alertText.style.color = 'green'
+        alertText.innerHTML = "Success !"
 
     }else{
-        document.getElementById("textalert").innerHTML = "Invalid username or password!"
+        alertText.innerHTML = "Invalid username or password!"
     }
 
 }
